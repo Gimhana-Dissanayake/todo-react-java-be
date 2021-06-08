abc1b867dce7593344aaf96da18760e0fcbbeb56
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-08 07:19:17 +0530

    testing apis

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -2,11 +2,13 @@ package com.example.controller;
 
 import com.example.domain.HelloBean;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
 
     @GetMapping(path = "/hello")
@@ -22,5 +24,10 @@ public class Controller {
     @GetMapping(path = "/hello-bean/path-variable/{name}")
     public HelloBean helloBeanPathVariable(@PathVariable String name) {
         return new HelloBean(String.format("Hello, %s", name));
+    }
+
+    @GetMapping(path = "/hello-error")
+    public HelloBean helloError() {
+        throw new RuntimeException("Something went wrong");
     }
 }
